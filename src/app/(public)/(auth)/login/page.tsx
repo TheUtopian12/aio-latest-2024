58c7e1ff5302d8a780d9b1df4a1c744cb8033550
Edwin <edwin@MacBook-Air-de-Edwin.local>
2024-11-20 11:53:38 -0600

    Se agrego login demo

--- src/app/(public)/(auth)/login/page.tsx
+++ src/app/(public)/(auth)/login/page.tsx
@@ -1,8 +1,23 @@
+"use client"
 import { Button, Input } from "@nextui-org/react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [login, setLogin] = useState("")
+  const [valid, setValid] = useState(false)
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const value = e.target[0].value;
+    setLogin(value);
+    if (value === "1234") {
+      window.location.href = "/main";
+    } else {
+      setValid(true);
+    }
+  }
+
+
   return (
     <div className="min-h-screen flex justify-center items-center bg-black">
       <section className="place-items-center space-y-10 w-96 h-max lg:w-[500px] py-16">
@@ -14,20 +29,25 @@ const Login = () => {
           height={100}
         />
         <p className=" text-xl font-bold text-white">Enter the 4-digit code</p>
-        <Input
-          type="text"
-          variant="bordered"
-          color="warning"
-          label="EX: 1234"
-          className="max-w-xs text-white"
-        />
-        <Button
-          radius="lg"
-          size="lg"
-          className="bg-[#F26457] text-white shadow-lg font-semibold"
-        >
-          Log In
-        </Button>
+        <form onSubmit={handleLogin} className="place-items-center space-y-5">
+          <Input
+            type="text"
+            variant="bordered"
+            color="warning"
+            label="EX: 1234"
+            isInvalid={valid}
+            errorMessage={"Invalid Digit"}
+            className="max-w-xs text-white text-center"
+          />
+          <Button
+            radius="lg"
+            size="lg"
+            type="submit"
+            className="bg-[#F26457] text-white shadow-lg font-semibold"
+          >
+            Log In
+          </Button>
+        </form>
       </section>
     </div>
   );
